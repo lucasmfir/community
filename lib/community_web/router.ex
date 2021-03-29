2354809ac1495a4b6d3daf30d860980daab4d0cc
Lucas Firmo <lucasmfir@gmail.com>
2021-03-28 23:24:45 -0300

    list news query

--- lib/community_web/router.ex
+++ lib/community_web/router.ex
@@ -9,6 +9,15 @@ defmodule CommunityWeb.Router do
     pipe_through :api
   end
 
+  scope "/" do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.Endpoint}
+  end
+
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
